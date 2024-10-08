ca9aa62ce1079feecb8745ef4fd09a9195b46532
DESKTOP-855CBKE\ibiz <gwonheehan@ibizsoftware.net>
2024-10-08 15:35:56 +0900

    서버 시간대 Asiz/Seoul 로 변경

--- src/main/java/com/example/drugtrack/DrugTrackApplication.java
+++ src/main/java/com/example/drugtrack/DrugTrackApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 @EnableScheduling
 public class DrugTrackApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(DrugTrackApplication.class, args);
 	}
 
