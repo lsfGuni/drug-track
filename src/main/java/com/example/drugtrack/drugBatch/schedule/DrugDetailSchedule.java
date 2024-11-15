655e707f9c82e732acdcd70aed863cd676c06c43
DESKTOP-855CBKE\ibiz <gwonheehan@ibizsoftware.net>
2024-11-15 17:33:11 +0900

    크론식 비활성화

--- src/main/java/com/example/drugtrack/drugBatch/schedule/DrugDetailSchedule.java
+++ src/main/java/com/example/drugtrack/drugBatch/schedule/DrugDetailSchedule.java
@@ -1,14 +1,8 @@
 package com.example.drugtrack.drugBatch.schedule;
 
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 /**
  * DrugDetailSchedule 클래스는 Spring의 스케줄러를 사용하여 특정 시간에 배치 작업을 자동으로 실행하는 설정을 제공합니다.
  * 이 클래스는 배치 작업을 정기적으로 실행할 수 있도록 구성되며, 실행 시간은 cron 표현식을 통해 설정할 수 있습니다.
@@ -38,7 +32,7 @@ public class DrugDetailSchedule {
      * 이 메서드는 매달 1일과 15일에 실행되며, JobParameters에 현재 시간을 포함하여 배치를 실행합니다.
      *
      * @throws Exception 배치 작업 실행 중 발생할 수 있는 예외
-     */
+
     @Scheduled(cron = "0 0 0 1,15 * ?", zone = "Asia/Seoul") // 매 달 1일, 15일에 실행
     //@Scheduled(cron = "0 0 * * * ?", zone = "Asia/Seoul") // 매 시정각에 실행
     public void runFirstJob() throws Exception {
@@ -59,4 +53,5 @@ public class DrugDetailSchedule {
     // TODO: 이 스케줄러는 현재 사용 중이지 않습니다. 스케줄러를 활성화하려면 application.yml 파일에 다음과 같이 설정하세요.
     // scheduling:
     //   enabled: true
+    **/
 }
